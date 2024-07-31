75cd0f3f37ade39bb3672f7275c65d6f689f1cde
Dan Feng <d.feng@hotmail.com>
2024-07-31 14:54:26 +0000

    Add testing code to check if the project is working properly with static files

--- about/views.py
+++ about/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.generic import TemplateView # For test
+
 # Create your views here.
+
+# For test
+class About(TemplateView):
+    template_name = 'base.html'
