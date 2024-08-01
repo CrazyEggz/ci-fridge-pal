f8ea0aa09896c5527ad634c987bc2d521f26715a
Dan Feng <d.feng@hotmail.com>
2024-08-01 09:43:35 +0000

    Render signup form with crispy form

--- fridge_pal/settings.py
+++ fridge_pal/settings.py
@@ -56,6 +56,9 @@ INSTALLED_APPS = [
     'home',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
