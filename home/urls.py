0ac22a14a015d5b053b851e626a02f109ee52308
Dan Feng <d.feng@hotmail.com>
2024-08-01 14:53:15 +0000

    Create template for login page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 # For test 
 urlpatterns = [
     path('', views.Home.as_view(), name = 'home'),
     path('signup/', views.SignUp.as_view(), name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'home/login.html'), name = 'login'),
 ]
