17f7e7f3557d5034acae31bca69fe33286328716
Dan Feng <d.feng@hotmail.com>
2024-08-01 22:20:26 +0000

    Create model 'Item'

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.CharField(max_length=200)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    best_before = models.DateTimeField(null=True, blank=True)
+    use_by = models.DateTimeField(null=True, blank=True)
+    location = models.ForeignKey('Location')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
