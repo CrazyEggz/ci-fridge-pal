8a486ca8aff48f97a848c5f401019f66a123f704
Dan Feng <d.feng@hotmail.com>
2024-08-02 10:24:32 +0000

    Fix errors in model 'Item' and make migrations all models 'Item', 'Category', 'Location' and 'Unit'

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,8 +9,8 @@ class Item(models.Model):
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
     best_before = models.DateTimeField(null=True, blank=True)
     use_by = models.DateTimeField(null=True, blank=True)
-    location = models.ForeignKey('Location', on_delete=models.SET_NULL)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
