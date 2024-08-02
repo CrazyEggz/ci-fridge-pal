c3f7737b85709e8a8a7bf9bc2416ad9ecfc5c6e1
Dan Feng <d.feng@hotmail.com>
2024-08-02 09:45:49 +0000

    Add 'unit' model field to the 'Item' model

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     quantity = models.CharField(max_length=200)
+    unit = models.ForeignKey('unit', on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
     best_before = models.DateTimeField(null=True, blank=True)
     use_by = models.DateTimeField(null=True, blank=True)
