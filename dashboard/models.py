e2b198ca0e53426973c56a7e68e904a0dba3f677
Dan Feng <d.feng@hotmail.com>
2024-08-02 16:48:35 +0000

    Alter 'expiry_date' and 'quantity' field on model 'Item'

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,10 +7,10 @@ AREA = ((0, 'Fridge'), (1, 'Freezer'))
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=200)
-    quantity = models.CharField(max_length=200)
+    quantity = models.IntegerField()
     unit = models.ForeignKey('unit', on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
-    expiry_date = models.DateTimeField(null=True, blank=True)
+    expiry_date = models.DateField(null=True, blank=True)
     expiry_date_type = models.IntegerField(choices=EXPIRY_DATE_TYPE, null=True, blank=True)
     location = models.IntegerField(choices=AREA)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
