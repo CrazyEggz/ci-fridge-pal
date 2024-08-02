818d5ffb074db1613e7c6b54bef0baa641976b79
Dan Feng <d.feng@hotmail.com>
2024-08-02 13:04:23 +0000

    Create basic template and view for dashboard and wire them up

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View
+from .models import Item
 
 # Create your views here.
+class Dashboard(View):
+    def get(self, request):
+        return render(request, 'dashboard/dashboard.html')
