41bbc06cec4daa3c525b79582f065e457456b23f
Dan Feng <d.feng@hotmail.com>
2024-08-06 11:59:29 +0000

    Link delete button in inventory table with corresponding item's url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.Dashboard.as_view(), name = 'dashboard'),
     path('dashboard/add-item', views.AddItem.as_view(), name = 'add-item'),
-    path('dashboard/edit-item/<int:pk>', views.EditItem.as_view(), name = 'edit-item')
+    path('dashboard/edit-item/<int:pk>', views.EditItem.as_view(), name = 'edit-item'),
+    path('dashboard/delete-item/<int:pk>', views.DeleteItem.as_view(), name = 'delete-item')
 ]
