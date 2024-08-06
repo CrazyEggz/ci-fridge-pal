f8c293cef172d3f837ea2c71d95513df32d3801e
Dan Feng <d.feng@hotmail.com>
2024-08-06 17:45:29 +0000

    Format code and initialise popovers

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,12 +1,17 @@
-let deleteItemModal = document.getElementById('deleteItemModal');
+// Initialise popovers
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
+
+let deleteItemModal = document.getElementById('deleteItemModal');
 deleteItemModal.addEventListener('show.bs.modal', function (event) {
   let button = event.relatedTarget;
-  let deleteItemUrl = button.getAttribute('data-bs-delete-url');
-  let deleteItemName = button.getAttribute('data-bs-item-name');
-  let modalDeleteButton = deleteItemModal.querySelector('#model-delete-btn');
-  let modalTitle = deleteItemModal.querySelector('.modal-title')
 
+  let modalDeleteButton = deleteItemModal.querySelector('#model-delete-btn');
+  let deleteItemUrl = button.getAttribute('data-bs-delete-url');
   modalDeleteButton.action = deleteItemUrl;
-  modalTitle.textContent = `Are you sure you want to delete the item ${deleteItemName}?`
+
+  let modalTitle = deleteItemModal.querySelector('.modal-title');
+  let deleteItemName = button.getAttribute('data-bs-item-name');
+  modalTitle.textContent = `Are you sure you want to delete the item ${deleteItemName}?`;
 });
