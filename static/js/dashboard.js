133a780f195f9ad77f060141df5ef5ff547cb66f
Dan Feng <d.feng@hotmail.com>
2024-08-06 16:49:52 +0000

    Fix the delete item modal window to display the item name

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -3,7 +3,10 @@ let deleteItemModal = document.getElementById('deleteItemModal');
 deleteItemModal.addEventListener('show.bs.modal', function (event) {
   let button = event.relatedTarget;
   let deleteItemUrl = button.getAttribute('data-bs-delete-url');
+  let deleteItemName = button.getAttribute('data-bs-item-name');
   let modalDeleteButton = deleteItemModal.querySelector('#model-delete-btn');
+  let modalTitle = deleteItemModal.querySelector('.modal-title')
 
   modalDeleteButton.action = deleteItemUrl;
+  modalTitle.textContent = `Are you sure you want to delete the item ${deleteItemName}?`
 });
