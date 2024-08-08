66948ea1eb2659f74260a562294b21f46e4111d2
Dan Feng <d.feng@hotmail.com>
2024-08-08 12:28:40 +0000

    Add three buttons to the inventory table to filter items according to their locations

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -15,3 +15,21 @@ deleteItemModal.addEventListener('show.bs.modal', function (event) {
   let deleteItemName = button.getAttribute('data-bs-item-name');
   modalTitle.textContent = `Are you sure you want to delete the item ${deleteItemName}?`;
 });
+
+
+/**
+ * Set the URL parameter when the button is clicked.
+ */
+function onParameterButtonClick(event) {
+  let parameterKey = event.target.getAttribute('data-parameter-key');
+  let parameterValue = event.target.getAttribute('data-parameter-value');
+  let params = new URLSearchParams(window.location.search);
+  params.set(parameterKey, parameterValue);
+  window.location.search = params.toString();
+}
+
+// Set up parameter buttons
+const parameterButtons = document.getElementsByClassName('parameter-btn')
+for (let button of parameterButtons) {
+  button.addEventListener('click', onParameterButtonClick);
+}
