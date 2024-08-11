b9b788ddc5a66dc48ccb3862478063530cbd99d8
Dan Feng <d.feng@hotmail.com>
2024-08-11 21:38:42 +0000

    Style about page: add buttons to show and hide text content

---
+++ static/js/about.js
@@ -0,0 +1,19 @@
+const aboutCardButtons = document.querySelectorAll('.about-card button');
+
+for (let button of aboutCardButtons) {
+    button.addEventListener('click', showContent)
+}
+
+/**
+ * Get the about content which had the same parent element as the button clicked
+ * Switch the class 'opaque' on and off for the about content
+ */
+function showContent() {
+    const aboutContent = this.parentElement.getElementsByClassName('about-content')[0];
+    aboutContent.classList.toggle('opaque');
+    if (this.innerText === 'Read more') {
+        this.innerText = 'Hide text';
+    } else {
+        this.innerText = 'Read more';
+    }
+}
