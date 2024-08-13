be599e7df05329246b85852ea895f37b7d5fb58f
Dan Feng <d.feng@hotmail.com>
2024-08-13 17:19:30 +0000

    Style signup page

--- home/forms.py
+++ home/forms.py
@@ -8,3 +8,6 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+        }
