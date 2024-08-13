4b8809fcab7d1bd690fbe1ad912f65983ad6f27e
Dan Feng <d.feng@hotmail.com>
2024-08-13 17:03:16 +0000

    Redirect user from login page to dashboard if already logged in

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.Home.as_view(), name = 'home'),
     path('signup/', views.SignUp.as_view(), name = 'signup'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'home/login.html'), name = 'login'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'home/login.html', redirect_authenticated_user = True), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'home/logout.html'), name = 'logout'),
 ]
